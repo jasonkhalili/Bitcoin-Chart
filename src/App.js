2817130cd699f0ed35d81a11791a84fa50c4a131
jasonk <jasonkhalili@gmail.com>
2021-10-30 22:29:51 -0700

    Added basic material layout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
+import React from 'react';
+import { AppBar,
+         Box,
+         Toolbar, 
+         Typography,
+         CssBaseline,
+         Grid,
+         Card
+        } from '@mui/material';
+
 const App = () => {
   return (
-    <h1>It works!</h1>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Bitcoin Chart
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Card>
+            test
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            test
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
