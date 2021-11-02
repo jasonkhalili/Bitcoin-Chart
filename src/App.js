40357d969fe7339d7e9d058a01f4cd04c0795c79
jasonk <jasonkhalili@gmail.com>
2021-11-01 21:27:45 -0700

    Added date picker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar,
          Box,
          Toolbar, 
@@ -7,8 +7,14 @@ import { AppBar,
          Grid,
          Card
         } from '@mui/material';
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const App = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -24,7 +30,10 @@ const App = () => {
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Card>
-            test
+            Start Date
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            End Date
+            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
           </Card>
         </Grid>
         <Grid item xs={6}>
